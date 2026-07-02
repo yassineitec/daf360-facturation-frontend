538e03358879e5ccb294a756a1479f26de32b83d
khalil rebhi <khalil.rebhi@itecgroupe.com>
2026-07-02 10:26:50 +0100

    fix: update @khalilrebhiitec/daf360 version to 3.2.0

--- federation.config.js
+++ federation.config.js
@@ -45,7 +45,7 @@ module.exports = withNativeFederation({
     '@khalilrebhiitec/daf360': {
       singleton: true,
       strictVersion: false,
-      requiredVersion: '3.1.1',
+      requiredVersion: '3.2.0',
     },
     '@ngrx/store': {
       singleton: true,
